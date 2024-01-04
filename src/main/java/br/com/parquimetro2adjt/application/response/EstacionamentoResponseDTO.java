2fb47c9204cb82607bd5bd5509220814efbe2a5e
cesarvecchio <ozaro.cesar@gmail.com>
2024-01-04 20:25:10 -0300

    Adição das classes iniciais do processo de estacionamento

---
+++ src/main/java/br/com/parquimetro2adjt/application/response/EstacionamentoResponseDTO.java
@@ -0,0 +1,16 @@
+package br.com.parquimetro2adjt.application.response;
+
+import br.com.parquimetro2adjt.domain.entity.Veiculo;
+import br.com.parquimetro2adjt.domain.enums.TipoEstacionamentoEnum;
+
+import java.time.LocalDateTime;
+
+public record EstacionamentoResponseDTO (
+        String id,
+        TipoEstacionamentoEnum tipoEstacionamento,
+        Integer duracaoDesejada,
+        LocalDateTime horaInicial,
+        LocalDateTime horaFinal,
+        Veiculo veiculo
+){
+}
